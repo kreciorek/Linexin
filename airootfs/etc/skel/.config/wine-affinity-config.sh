38d70678e1dedfca586a84c0d0fe585eafb2e625
Petexy <piotr.petexiness@gmail.com>
2025-07-22 10:34:23 +0200

    250722-2
    - Affinity Installer is now built-in the system

--- airootfs/etc/skel/.config/wine-affinity-config.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-rum wine-affinity "$HOME/.WineAffinity" winetricks dotnet48 corefonts
-mkdir -p "$HOME/.WineAffinity/drive_c/windows/system32/WinMetadata"
-git clone https://github.com/daniel080400/AffinityLinuxTut
-cd AffinityLinuxTut
-unzip WinMetadata.zip
-cp -rf WinMetadata "$HOME/.WineAffinity/drive_c/windows/system32/WinMetadata"
-rum wine-affinity "$HOME/.WineAffinity" wine winecfg -v win11
---
+++ airootfs/etc/skel/.local/share/linexin/scripts/wine-affinity-config.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+rum wine-affinity "$HOME/.WineAffinity" winetricks dotnet48 corefonts
+mkdir -p "$HOME/.WineAffinity/drive_c/windows/system32/WinMetadata"
+git clone https://github.com/daniel080400/AffinityLinuxTut
+unzip AffinityLinuxTut/WinMetadata.zip
+cp -rf AffinityLinuxTut/WinMetadata $HOME"/.WineAffinity/drive_c/windows/system32/WinMetadata"
+rum wine-affinity $HOME"/.WineAffinity" wine winecfg -v win11
+rum affinity-photo3-wine9.13-part3 /home/petexy/.wineAffinity winetricks renderer=vulkan
+rm -rf AffinityLinuxTut
