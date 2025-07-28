0f1b31e9b3b3738038d3a766288bdbc644e31d3f
Petexy <piotr.petexiness@gmail.com>
2025-07-28 22:21:25 +0200

    250728-2
    - Quick hotfix for Affinity suite

--- airootfs/etc/skel/.local/share/linexin/scripts/wine-affinity-config.sh
+++ airootfs/etc/skel/.local/share/linexin/scripts/wine-affinity-config.sh
@@ -4,7 +4,7 @@ rum wine-affinity "$HOME/.WineAffinity" winetricks dotnet48 corefonts
 mkdir -p "$HOME/.WineAffinity/drive_c/windows/system32/WinMetadata"
 git clone https://github.com/daniel080400/AffinityLinuxTut
 unzip AffinityLinuxTut/WinMetadata.zip
-cp -rf AffinityLinuxTut/WinMetadata $HOME"/.WineAffinity/drive_c/windows/system32/WinMetadata"
 rum wine-affinity $HOME"/.WineAffinity" wine winecfg -v win11
+cp -rf AffinityLinuxTut/WinMetadata $HOME"/.WineAffinity/drive_c/windows/system32"
 rum affinity-photo3-wine9.13-part3 /home/petexy/.wineAffinity winetricks renderer=vulkan
 rm -rf AffinityLinuxTut
